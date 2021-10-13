878425e2aaf497728e73796511c49354d488d0c0
Eduardo Varriale da Silva <eduardo.silva@altus.com.br>
2021-10-13 15:28:47 -0300

    Added PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -15,6 +15,9 @@ namespace xadrez_console
                 tab.ColocarPeca(new Torre(Cor.Branca, tab), new Posicao(1, 3));
                 tab.ColocarPeca(new Rei(Cor.Branca, tab), new Posicao(0, 2));
                 Console.WriteLine(tab);
+
+                PosicaoXadrez pos = new PosicaoXadrez('C', 7);
+                Console.WriteLine(pos.toPosicao());
             }
             catch (TabuleiroException e)
             {
---
+++ xadrez-console/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna.ToString().ToLower()[0];
+            Linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+    }
+}
